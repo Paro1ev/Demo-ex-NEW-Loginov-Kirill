507d35286acaefa735f48020399e97eaec22aacd
Parolev <kirill.loginov1995@mail.ru>
2024-11-24 23:40:46 +0500

    Add Class Order and DTO

--- Program.cs
+++ Program.cs
@@ -4,3 +4,31 @@ var app = builder.Build();
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
+
+
+class Order
+{
+    public Order(int number, DateOnly startDate, string device, string problemType, string desciption, string client, string status)
+    {
+        Number = number;
+        StartDate = startDate;
+        Device = device;
+        ProblemType = problemType;
+        Desciption = desciption;
+        Client = client;
+        Status = status;
+    }
+
+    public int Number { get; set; }
+    public DateOnly StartDate { get; set; }
+    public string Device {  get; set; }
+    public string ProblemType { get; set; }
+    public string Desciption { get; set; }
+    public string Client {  get; set; }
+    public string Status { get; set; }
+    public DateOnly? EndDate { get; set; } = null;
+    public string? Master { get; set; } = "не назначен";
+    public List<string> Comments { get; set; } = [];
+}
+
+record class UpdateOrderDTO(int Number, string? Status, string? Description, string? Master, string? Comment);
